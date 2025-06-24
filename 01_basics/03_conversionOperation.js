b33ca4b7802730d834a035127a29f0760ae14b81
Rish-0 <luciferhnerf@gmail.com>
2025-06-24 16:41:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score="hitesh"//null//"33bc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" =>33
+//"33bc" =>NaN
+//true =>1; false =>0
+
+let IsLoggedIn="bbb"//1
+
+let booleanIsLoggedIn=Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 =>false
+//"" =>false
+//"bbb" =>true
+
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
